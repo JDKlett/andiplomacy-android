e9f8d641d7cbcec7a05d8009e157fe469b711222
jdklett <kekkolett@gmail.com>
2017-06-04 22:54:36 +0100

    Added Alarms and Service to check

---
+++ mobile/src/main/java/app/android/vdiplomacy/org/andiplomacy/CheckerService.java
@@ -0,0 +1,106 @@
+package app.android.vdiplomacy.org.andiplomacy;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import org.vdiplomacy.android.DiplomacyAuthenticationException;
+import org.vdiplomacy.android.DiplomacyClient;
+
+import java.util.List;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * helper methods.
+ */
+public class CheckerService extends IntentService {
+
+    private static final String ACTION_DISMISS = "app.android.vdiplomacy.org.andiplomacy.action.DISMISS";
+    private static final String ACTION_START = "app.android.vdiplomacy.org.andiplomacy.action.START";
+    private static final String EXTRA_ID = "app.android.vdiplomacy.org.andiplomacy.extra.id";
+    private static List<Integer> matches = null;
+    private boolean isNotificationShown = false;
+
+    private int mInterval = 300;
+    public CheckerService(){
+        super("Checker Service");
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    public static Intent createActionStart(Context context) {
+        Intent intent = new Intent(context, CheckerService.class);
+        intent.setAction(ACTION_START);
+        return intent;
+    }
+
+    public static void startActionStart(Context context) {
+        context.startService(createActionStart(context));
+    }
+
+    public static Intent createActionDismiss(Context context) {
+        Intent intent = new Intent(context, CheckerService.class);
+        intent.setAction(ACTION_DISMISS);
+        return intent;
+    }
+
+    public static void startActionDismiss(Context context) {
+        context.startService(createActionDismiss(context));
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_START.equals(action)) {
+                try {
+                    if(matches == null){
+                        matches = DiplomacyClient.getMatches();
+                    }
+                    for(Integer id: matches){
+                        if(!isNotificationShown && DiplomacyClient.checkMessages(id)){
+                            showNotification(this);
+                        }
+                    }
+                } catch (DiplomacyAuthenticationException e) {
+                    Log.e(getClass().getName(), e.getMessage());
+                }
+            } else if (ACTION_DISMISS.equals(action)) {
+                isNotificationShown = false;
+            }
+        }
+    }
+
+    private void showNotification(Context context){
+        Intent dismissNotificationIntent = CheckerService.createActionDismiss(context);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, dismissNotificationIntent, PendingIntent.FLAG_ONE_SHOT);
+        Log.d("DEBUG", "Pending event created.");
+        isNotificationShown = true;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_content))
+                .setSmallIcon(R.drawable.ic_message)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setAutoCancel(true)
+                .setDeleteIntent(pendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, builder.build());
+    }
+}
--- mobile/src/main/java/app/android/vdiplomacy/org/andiplomacy/Dashboard.java
+++ mobile/src/main/java/app/android/vdiplomacy/org/andiplomacy/Dashboard.java
@@ -1,121 +1,54 @@
 package app.android.vdiplomacy.org.andiplomacy;
 
-import android.app.Activity;
-import android.app.NotificationManager;
+import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.util.Log;
-
-import org.vdiplomacy.android.DiplomacyAuthenticationException;
-import org.vdiplomacy.android.DiplomacyClient;
-
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
 
-    private int mInterval = 300;
-    private boolean isNotificationShown = false;
-    private BroadcastReceiver broadcastReceiver = null;
-    private BroadcastReceiver dismissReceiver = null;
-    private CheckMessagesTask cmTask = null;
+    private int mInterval = 300000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                isNotificationShown = false;
-            }
-        };
-        registerReceiver(broadcastReceiver, new IntentFilter("NOTIFICATION_DISMISSED"));
-
-        dismissReceiver = new OnNotificationDismissed();
-        registerReceiver(dismissReceiver, new IntentFilter("NOTIFICATION_DELETE_REQUEST"));
-
-        cmTask = new CheckMessagesTask();
-        cmTask.execute((Void) null);
+        scheduleCheck();
     }
 
-    private class MessageChecker implements Runnable {
-
-        private int id = 0;
-
-        public MessageChecker(int id) {
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Log.d(getClass().getName(), "Checking presence of messages for game["+id+"]");
-                if(!isNotificationShown && DiplomacyClient.checkMessages(id)){
-                    Log.d(getClass().getName(), "Game ["+id+"] has new messages.");
-                    showNotification();
-                }
-            } catch (Throwable e) {
-                //DO NOTHING
-            }
-        }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+//        scheduleCheck();
     }
 
-    private void showNotification(){
-        Intent dismissNotificationIntent = new Intent("NOTIFICATION_DELETE_REQUEST");
-        isNotificationShown = true;
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 0, dismissNotificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        Log.d("DEBUG", "Pending event created.");
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this.getApplicationContext());
-        builder
-                .setContentTitle(getString(R.string.notification_title))
-                .setContentText(getString(R.string.notification_content))
-                .setSmallIcon(R.drawable.ic_message)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setAutoCancel(true)
-                .setDeleteIntent(pendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(0, builder.build());
-    }
-
-    public class CheckMessagesTask extends AsyncTask<Void, Void, Boolean> {
-
-        public CheckMessagesTask() {
-
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            try {
-                List<Integer> matches = DiplomacyClient.getMatches();
-                for(Integer id: matches){
-                    ScheduledExecutorService scheduler =
-                            Executors.newSingleThreadScheduledExecutor();
-                    scheduler.scheduleAtFixedRate(new MessageChecker(id),  5, mInterval, TimeUnit.SECONDS);
-                    Log.i(getClass().getName(), "Created scheduler for match id:"+id);
-                }
-            } catch (DiplomacyAuthenticationException e) {
-                return false;
-            }
-            return true;
-        }
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        cancelCheck();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(broadcastReceiver);
+        cancelCheck();
+    }
+
+
+    private void scheduleCheck(){
+        Intent intent = CheckerService.createActionStart(this);
+        PendingIntent checkIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+        SystemClock.elapsedRealtime()+10, mInterval, checkIntent);
+    }
+
+    private void cancelCheck(){
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent intent = CheckerService.createActionStart(this);
+        PendingIntent checkIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        am.cancel(checkIntent);
     }
 }
